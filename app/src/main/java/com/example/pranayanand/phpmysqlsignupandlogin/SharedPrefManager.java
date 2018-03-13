cbeb38bdb0184695252aae6344666fad6c3c5c30
Pranay Anand <pranayanand123@gmail.com>
2018-03-13 13:47:44 +0530

    integer

--- app/src/main/java/com/example/pranayanand/phpmysqlsignupandlogin/SharedPrefManager.java
+++ app/src/main/java/com/example/pranayanand/phpmysqlsignupandlogin/SharedPrefManager.java
@@ -12,6 +12,7 @@ public class SharedPrefManager {
 
     //the constants
     int a = 20;
+    int b = 30;
     private static final String SHARED_PREF_NAME = "simplifiedcodingsharedpref";
     private static final String KEY_USERNAME = "keyusername";
     private static final String KEY_EMAIL = "keyemail";
