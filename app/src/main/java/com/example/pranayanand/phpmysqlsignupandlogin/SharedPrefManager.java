a211cd7f309c847f5f569493668c6d57b8743a87
Pranay Anand <pranayanand123@gmail.com>
2018-03-13 13:44:14 +0530

    int

--- app/src/main/java/com/example/pranayanand/phpmysqlsignupandlogin/SharedPrefManager.java
+++ app/src/main/java/com/example/pranayanand/phpmysqlsignupandlogin/SharedPrefManager.java
@@ -11,6 +11,7 @@ import android.content.SharedPreferences;
 public class SharedPrefManager {
 
     //the constants
+    int a = 20;
     private static final String SHARED_PREF_NAME = "simplifiedcodingsharedpref";
     private static final String KEY_USERNAME = "keyusername";
     private static final String KEY_EMAIL = "keyemail";
